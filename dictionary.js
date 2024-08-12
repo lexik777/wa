1bb1627ce238fb60006681e94bf31ffd2fd1be95
lexik777 <laixi.kang@mail.mcgill.ca>
2024-08-12 13:01:17 -0700

    Add files via upload

---
+++ dictionary.js
@@ -0,0 +1,18 @@
+const dictionary = words.map((group) => {
+  return Object.keys(group)
+}).flat()
+
+// const dictionary = [
+//   'apple',
+//   'banana',
+//   'cherry',
+//   'date',
+//   'grape',
+//   'kiwi',
+//   'lemon',
+//   'mango',
+//   'orange',
+//   'pear',
+//   'plum',
+//   'strawberry'
+// ];
